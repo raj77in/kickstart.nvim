fc646590d52f0a38860980ec298e02b66d6cb32b
Amit Agarwal <gmail@amit-agarwal.co.in>
2024-02-05 10:19:14 +0530

    more changes.

--- lua/util/modified.lua
+++ lua/util/modified.lua
@@ -13,15 +13,15 @@ M.changeheader = function()
 	--     require("notify")("Current file has not changed!")
 	--     return
 	-- end
-	if vim.fn.line("$") >= 7 then
+	if vim.fn.line("$") >= 27 then
 		os.setlocale("en_US.UTF-8") -- show Sun instead of dom (portuguese)
 		local time = os.date("%a, %d %b %Y %H:%M:%S")
 		local l = 1
-		while l <= 7 do
-			vim.fn.setline(l, vim.fn.substitute(vim.fn.getline(l), 'last change: \\zs.*', time, 'gc'))
+		while l <= 27 do
+			vim.fn.setline(l, vim.fn.substitute(vim.fn.getline(l), 'Last \(change|modified|update|updated\): \\zs.*', time, 'gci'))
 			l = l + 1
 		end
-		if vim.fn.search('\\v?ast (?hange|?pdate|?pdated):?', 'wncp') > 0 then
+		if vim.fn.search('\\vLast (?hange|?pdate|?pdated|?odified):?', 'wncp') > 0 then
 			require("notify")("Changed file header!")
 		end
 	end

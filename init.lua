88edbf6b72b2ab6c2b0079762ad43d25e15374b7
Amit Agarwal <raj77in>
2024-07-28 13:57:47 +0530

    Add nvim surround.

--- init.lua
+++ init.lua
@@ -182,6 +182,15 @@ require('lazy').setup({
           return vim.fn.executable 'make' == 1
         end,
       },
+      {
+        'kylechui/nvim-surround',
+        -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+      },
     },
   },
 

bf85226ae3980d896fdccc89e25f2aa9d576a67a
Amit Agarwal <raj77in>
2024-12-25 16:33:59 +0530

    Fix abbreviations.

--- lua/custom/plugins/abbreviate.lua
+++ lua/custom/plugins/abbreviate.lua
@@ -1,8 +1,8 @@
 vim.cmd('abbr aka "Amit Agarwal"')
 vim.cmd('ab akae "Amit Agarwal <amit.agarwal@mobileum.com>"')
-vim.cmd('ab ` ```')
-vim.cmd('ab `g ```go')
-vim.cmd('ab `p ```python')
+vim.cmd('ab `g ```go\n')
+vim.cmd('ab `p ```python\n')
+vim.cmd('ab `b ```bash\n')
 vim.cmd('ab h1 #')
 vim.cmd('ab h2 ##')
 vim.cmd('ab h3 ###')
